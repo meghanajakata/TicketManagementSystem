f4209ffa61c0d67bc591a6717c72eaa909c3422b
DE001\sin15-sh <sin15-sh@de001.itgr.net>
2025-01-18 16:39:05 +0530

    Code changes for getting key vaults

--- TicketManagementSystem/Areas/Admin/Controllers/HomeController.cs
+++ TicketManagementSystem/Areas/Admin/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TicketManagementSystem.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private SecretClient _secretClient;
         public IActionResult Index()
         {
+            var key = "https://meghanakeyvault23.vault.azure.net/";
+            _secretClient = new SecretClient(new Uri(key), new DefaultAzureCredential());
+            var secretName = Environment.GetEnvironmentVariable("Name");
+            var secretValue = _secretClient.GetSecret(secretName);
+            ViewBag.Secret = secretValue.Value.Value;
             return View();
         }
     }
